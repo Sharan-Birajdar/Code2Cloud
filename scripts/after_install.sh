92ff3b614f7140c17a067dfb0fb2e303128d0463
Sharan-Birajdar <sharanrb143@gmail.com>
2026-04-08 13:57:36 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Pull latest image from ECR
-IMAGE_URI=$(cat /home/ubuntu/app/imageDetail.json | python3 -c "import sys,json; print(json.load(sys.stdin)['ImageURI'])")
+IMAGE_URI=$(cat /home/ubuntu/my-webapp/imageDetail.json | python3 -c "import sys,json; print(json.load(sys.stdin)['ImageURI'])")
 echo "Pulling image: $IMAGE_URI"
 docker pull $IMAGE_URI
-echo $IMAGE_URI > /home/ubuntu/app/current_image.txt
+echo $IMAGE_URI > /home/ubuntu/my-webapp/current_image.txt
