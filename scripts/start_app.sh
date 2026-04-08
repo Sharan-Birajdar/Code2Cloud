46be2c24efb30dc034ed56fdcf1e428ef8d68330
Sharan-Birajdar <sharanrb143@gmail.com>
2026-04-08 14:40:00 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-IMAGE_URI=$(cat /home/ubuntu/app/current_image.txt)
 
-# Run the container (adjust port as needed)
-docker run -d \
-  --name webapp \
-  --restart unless-stopped \
-  -p 80:3000 \
-  $IMAGE_URI
+echo "Stopping old container..."
 
-echo "Container started successfully"
+docker stop myapp || true
+docker rm myapp || true
+
+echo "Pulling latest image..."
+
+docker pull 654485376127.dkr.ecr.ap-south-1.amazonaws.com/my-webapp:latest
+
+echo "Starting new container..."
+
+docker run -d -p 80:3000 --name myapp 654485376127.dkr.ecr.ap-south-1.amazonaws.com/my-webapp:latest
