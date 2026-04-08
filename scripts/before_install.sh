5ec4c22a0d30490e3428007997727e041348c006
Sharan-Birajdar <sharanrb143@gmail.com>
2026-04-08 13:36:10 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Stop old container (ignore error)
+# Install AWS CLI (force install every time - safe)
+sudo apt update -y
+sudo apt install awscli -y
+
+# Stop old container
 docker stop webapp || true
 docker rm webapp || true
 
-# Login to ECR (CORRECT WAY)
+# Login to ECR (correct method)
 aws ecr get-login-password --region ap-south-1 \
 | docker login --username AWS --password-stdin 654485376127.dkr.ecr.ap-south-1.amazonaws.com
