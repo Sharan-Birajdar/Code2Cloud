d795ce143c8a0571b7305f6d1c8238b96f62c879
Sharan-Birajdar <sharanrb143@gmail.com>
2026-04-08 13:24:18 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Stop and remove existing container
+
+# Stop old container (ignore error)
 docker stop webapp || true
 docker rm webapp || true
 
-# Login to ECR
-aws ecr get-login-password --region ap-south-1 | \
-  docker login --username AWS --password-stdin \
-  123456789.dkr.ecr.ap-south-1.amazonaws.com
+# Login to ECR (CORRECT WAY)
+aws ecr get-login-password --region ap-south-1 \
+| docker login --username AWS --password-stdin 654485376127.dkr.ecr.ap-south-1.amazonaws.com
